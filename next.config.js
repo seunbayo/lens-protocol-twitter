a2c7080a880476fd2d2667902ae47b80552792b6
seunbayo <seunbayo83@gmail.com>
2022-11-29 07:49:18 +0100

    made changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  reactStrictMode: true,
+  productionBrowserSourceMaps: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  trailingSlash: true,
+  assetPrefix: isProd ? process.env.NEXT_PUBLIC_SITE_URL : "",
+};
